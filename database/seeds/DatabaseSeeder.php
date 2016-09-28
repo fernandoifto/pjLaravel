7af3d9ba29cf80f9ffc474a64a7404f1ec924fa7
fernando.arantes <fernando.arantes@ifto.edu.br>
2016-09-28 17:59:34 -0300

    Criação de seeds e Factory Fake

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        
+        \pjLaravel\Client::truncate();
+        
+        factory(\pjLaravel\Client::class, 5)->create();
 
         // $this->call(UserTableSeeder::class);
 
