ccfb4711d4359355188dbd9dbc6d528d8db1a473
fernando.arantes <fernando.arantes@ifto.edu.br>
2016-10-14 17:00:14 -0300

    Criado a Classe ClientValidator

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -3,21 +3,50 @@
 namespace pjLaravel\Services;
 
 use pjLaravel\Repositories\ClientRepository;
+use pjLaravel\Validators\ClientValidator;
+use Prettus\Validator\Exceptions\ValidatorException;
 
 class ClientService {
     
-    protected $repository;
+    /**
+     *
+     * @var ClientRepository
+     */
+    /**
+     *
+     * @var ClientValidator
+     */
     
-    public function __construct(ClientRepository $repository) {
-        $this->repository = $repository;        
+    protected $repository;
+    protected $validator;
+
+    public function __construct(ClientRepository $repository, ClientValidator $validator) {
+        $this->repository = $repository;  
+        $this->validator = $validator;
     }
     
-    public function create(array $data){
-        return $this->repository->create($data);
+    public function create(array $data){  
+        try {
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->create($data);
+        } catch (ValidatorException $e) {
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        } 
     }
     
     public function update(array $data, $id){
-        return $this->repository->update($data, $id);
+        try {
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->update($data, $id);
+        } catch (ValidatorException $e) {
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        } 
     }
     
 }
